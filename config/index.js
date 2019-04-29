d323a5d3985796b13b5d682e83a3e0ecf8bbe5a2
Fernando La Chica <flachica@github.com>
2019-04-29 19:18:54 +0200

    [fix] Change URL

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://podemoslarinconada.github.io',
+    assetsPublicPath: 'https://podemoslarinco.github.io/podemoslarinconada/',
 
     /**
      * Source Maps
